fc443cdbe94e11a70a2eff73b73a909b6f6a2afd
harsh <harsh@DESKTOP-MSTPP8B>
2019-10-07 15:52:24 -0700

    initial adding machine java file

---
+++ cse360assign2/cse360assign2/AddingMachine.java
@@ -0,0 +1,39 @@
+package cse360assign2;
+
+/**
+ * Name: Harsha Sidda 
+ * Email: hsidda@asu.edu
+ * Class Section: 70642 (Wednesday)
+ * Assignment Description: This assignment helps us create a repository on Github and exports this
+   project there. This program tracks operations on a number.
+**/
+
+public class AddingMachine 
+{
+	private int total;
+	
+	public AddingMachine() 
+	{
+		total = 0;  // not needed - included for clarity
+	}
+	public int getTotal() 
+	{
+		return 0;
+	}
+	public void add(int value)
+	{
+		
+	}
+	public void subtract(int value) 
+	{
+		
+	}
+	public String toString() 
+	{
+		return "";
+	}
+	public void clear() 
+	{
+		
+	}
+}
